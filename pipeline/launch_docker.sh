9518ba701d440fd3e2d96aee91f6fa20b55a44e7
Frank Rouse <frank.rouse@dell.com>
2018-02-21 12:04:30 -0500

    Add docker launch script.

---
+++ pipeline/launch_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Set up docker daemon with a config file:
+docker_config(){
+	mkdir /etc/docker
+	touch /etc/docker/daemon.json
+	echo '{"storage-driver":"btrfs","debug":true}' > /etc/docker/daemon.json
+}
+
+# Start the docker daemon as a background task:
+start_daemon(){
+	dockerd --config-file=/etc/docker/daemon.json -p /var/run/docker-bootstrap.pid &
+}
+
+docker_config
+start_daemon
